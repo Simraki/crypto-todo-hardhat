c337a41e3ee76274af3c5d5d66981e3935f7af3c
Simraki <Simraki@mail.ru>
2022-05-20 21:41:03 +0700

    add contractAddress param to tasks

--- tasks/CryptoTodo.ts
+++ tasks/CryptoTodo.ts
@@ -1,11 +1,10 @@
 import { task } from "hardhat/config"
 
-const contractAddress = "0x245277ea63D22D396aC7bf6505969B89b977Fa1C"
-
 task("task", "get task")
     .addParam("id", "id of a necessary task")
+    .addParam("address", "contract address")
     .setAction(async (taskArgs, hre) => {
-        const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
         const res = await contract.task(taskArgs.id)
         console.log(res)
@@ -14,25 +13,29 @@ task("task", "get task")
 task("personal", "get personal task")
     .addParam("expired", "filter for returning expired tasks (true - return)")
     .addParam("removed", "filter for returning soft-removed tasks (true - return)")
+    .addParam("address", "contract address")
     .setAction(async (taskArgs, hre) => {
-        const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
         const res = await contract.personalTasks(taskArgs.expires, taskArgs.removed)
         console.log(res)
     })
 
-task("percentages", "get percentage of on-time completed tasks for each owner").setAction(async (taskArgs, hre) => {
-    const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+task("percentages", "get percentage of on-time completed tasks for each owner")
+    .addParam("address", "contract address")
+    .setAction(async (taskArgs, hre) => {
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
-    const res = await contract.onTimeTaskPercents()
-    console.log(res)
-})
+        const res = await contract.onTimeTaskPercents()
+        console.log(res)
+    })
 
 task("create", "create task with description and expiration date")
     .addParam("name", "description")
     .addParam("date", "expiration date (timestamp)")
+    .addParam("address", "contract address")
     .setAction(async (taskArgs, hre) => {
-        const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
         const tx = await contract.create(taskArgs.name, taskArgs.date)
         const rc = await tx.wait()
@@ -42,24 +45,27 @@ task("create", "create task with description and expiration date")
 
 task("toggle", "toggle task")
     .addParam("id", "id of a necessary task")
+    .addParam("address", "contract address")
     .setAction(async (taskArgs, hre) => {
-        const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
         await contract.toggle(taskArgs.id)
     })
 
 task("remove", "soft remove task")
     .addParam("id", "id of a necessary task")
+    .addParam("address", "contract address")
     .setAction(async (taskArgs, hre) => {
-        const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
         await contract.softRemove(taskArgs.id)
     })
 
 task("restore", "restore soft-removed task")
     .addParam("id", "id of a necessary task")
+    .addParam("address", "contract address")
     .setAction(async (taskArgs, hre) => {
-        const contract = await hre.ethers.getContractAt("CryptoTodo", contractAddress)
+        const contract = await hre.ethers.getContractAt("CryptoTodo", taskArgs.address)
 
         await contract.restore(taskArgs.id)
     })
